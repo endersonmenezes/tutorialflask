0051d73784079071e3736b7ddd5d06eb4971efbc
Enderson Menezes <endersonster@gmail.com>
2020-10-17 15:33:42 -0300

    Atualizando README.md

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request as flask_request
+
+from bs4 import BeautifulSoup
 
 import requests
 
@@ -19,6 +21,35 @@ def api_dolar():
     r_api_data = r_api.json()
     dolar_value = r_api_data['results']['currencies']['USD']['buy']
     return jsonify({'dolar': dolar_value})
+
+
+@app.route("/wikipedia")
+def api_wikipedia():
+    """
+    Rota que devolve dados de uma página no wikipedia.
+    :return:  jsonify
+    """
+    search = flask_request.args.get('search', None)
+    url = flask_request.url
+    if search:
+        api_wikipedia = 'https://pt.wikipedia.org/wiki/{}'.format(search)
+        r_api = requests.get(api_wikipedia)
+        r_api_bs = BeautifulSoup(r_api.text, 'html.parser')
+        title = r_api_bs.find(id='firstHeading')
+        body = r_api_bs.find(id='bodyContent')
+        div_class_table = r_api_bs.find_all('div', attrs={'class': 'mw-parser-output'})
+        div_class_table = div_class_table[0]
+        p_from_table = div_class_table.find('p')
+        return jsonify({
+            'titulo': title.text,
+            'corpo': p_from_table.text,
+        })
+    else:
+        return jsonify({
+            'detail': 'Erro, você precisa informar um parametro de busca!',
+            'example': '{}?search=<SUA_BUSCA_AQUI>'.format(url)
+        })
+
 
 @app.route("/json")
 def json_example():
