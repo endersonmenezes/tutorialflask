84f6751a3b393d9ce87d65f189e11c721d1bc3a8
Enderson Menezes <endersonster@gmail.com>
2020-10-20 20:03:49 -0300

    Testando migrates.

--- models.py
+++ models.py
@@ -15,3 +15,16 @@ class Teste(db.Model):
     def __repr__(self):
         return 'Email:{} <{}>'.format(self.email, self.id)
 
+
+class Teste2(db.Model):
+    __tablename__ = 'teste_db_2'
+
+    id = db.Column(db.Integer, primary_key=True)
+    endereco = db.Column(db.String(120), index=True, unique=True)
+
+    def __init__(self, endereco):
+        self.endereco = endereco
+
+    def __repr__(self):
+        return 'Endereço: {} <{}>'.format(self.endereco, self.id)
+
